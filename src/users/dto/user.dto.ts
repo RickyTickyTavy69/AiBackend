ae1f2106fecf583810a586579b7bae79c4cf4e77
artem <bakirovartem69@gmail.com>
2024-07-26 20:50:03 +0200

    changed routes

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -2,8 +2,6 @@ import {ApiProperty} from '@nestjs/swagger';
 
 export class UserDto{
     @ApiProperty()
-    id: string;
-    @ApiProperty()
     username: string;
     @ApiProperty()
     password: string;
